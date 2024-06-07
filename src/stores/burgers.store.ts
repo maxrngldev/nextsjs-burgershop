8376a1a3fae5fb0261ba7512591040f38e0f5fbf
maxrngldev <maxrngl08@gmail.com>
2024-06-07 16:33:45 -0600

    persisting data on sessionStorage

--- src/stores/burgers.store.ts
+++ src/stores/burgers.store.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 import { Burger } from '@/models/Burgers';
 import { CartItem } from '@/models/Cart';
 
@@ -20,34 +21,44 @@ export const initialState: BurgersState = {
   cart: [],
 };
 
+create();
+
 export function createBurgersStore() {
-  return create<BurgersStore>()((set) => ({
-    ...initialState,
-    setBurgers: (burgers) => set(() => ({ burgers })),
-    addBurgerToCart: (burger) =>
-      set((state) => {
-        // Check if burger exists on cart
-        const burgerInCart = state.cart.find(
-          (cartItem) => cartItem.id === burger.id
-        );
+  return create<BurgersStore>()(
+    persist(
+      (set) => ({
+        ...initialState,
+        setBurgers: (burgers) => set(() => ({ burgers })),
+        addBurgerToCart: (burger) =>
+          set((state) => {
+            // Check if burger exists on cart
+            const burgerInCart = state.cart.find(
+              (cartItem) => cartItem.id === burger.id
+            );
 
-        if (burgerInCart) {
-          const updatedCartItems: CartItem[] = state.cart.map((cartItem) =>
-            cartItem.id === burgerInCart.id
-              ? { ...cartItem, quantity: ++cartItem.quantity }
-              : { ...cartItem }
-          );
+            if (burgerInCart) {
+              const updatedCartItems: CartItem[] = state.cart.map((cartItem) =>
+                cartItem.id === burgerInCart.id
+                  ? { ...cartItem, quantity: ++cartItem.quantity }
+                  : { ...cartItem }
+              );
 
-          return { ...state, cart: updatedCartItems };
-        } else {
-          // If not, add it to the cart
-          const newCartItems: CartItem[] = [
-            ...state.cart,
-            { ...burger, quantity: 1 },
-          ];
-          return { ...state, cart: newCartItems };
-        }
+              return { ...state, cart: updatedCartItems };
+            } else {
+              // If not, add it to the cart
+              const newCartItems: CartItem[] = [
+                ...state.cart,
+                { ...burger, quantity: 1 },
+              ];
+              return { ...state, cart: newCartItems };
+            }
+          }),
+        removeBurgerFromCart: (burger) => set(() => ({ burgers: [] })),
       }),
-    removeBurgerFromCart: (burger) => set(() => ({ burgers: [] })),
-  }));
+      {
+        name: 'burgers-store',
+        storage: createJSONStorage(() => sessionStorage),
+      }
+    )
+  );
 }
